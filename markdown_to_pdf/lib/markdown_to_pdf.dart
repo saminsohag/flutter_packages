65ffce444a2858677572bc9763582da69db20ce3
saminsohag <sssohag172@gmail.com>
2023-04-07 22:45:37 +0600

    bug fixed

--- markdown_to_pdf/lib/markdown_to_pdf.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-import 'package:markdown/markdown.dart' as markdown;
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pdf;
-
-void convert(String text) {
-  final file = File(inputPath);
-  final markdownText = file.readAsStringSync();
-}
